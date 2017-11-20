b7d6ec64dead5bea484b2a3774344f87ba75a258
Nelson Catalano <nelson.catalano88@gmail.com>
2017-11-20 16:04:41 +0100

    js Clock
    Project finish

--- JsClock/Scripts/clock.js
+++ JsClock/Scripts/clock.js
@@ -5,8 +5,15 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date()
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
     const secondsDeg = ((seconds / 60) * 360) + 90;
+    const minuteDeg = ((minutes / 60) * 360) + 90;
+    const hourDeg = ((hours / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
 }
+
 setInterval(setDate, 1000);
+setDate();
